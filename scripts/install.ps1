f09d5c95235db971b949601bdea1f9ac825f3d0f
arki <antoniorafaelariasromero@gmail.com>
2025-02-22 16:17:38 -0500

    chore: add scripts directory and versions.json for project management
    - Added `scripts/` directory for auxiliary scripts to support development and automation.
    - Included `versions.json` to track project versioning and dependencies.

---
+++ scripts/install.ps1
@@ -0,0 +1,31 @@
+$jsonUrl = "https://raw.githubusercontent.com/arkeasz/script-autoclick/main/versions.json"
+
+try {
+    $json = Invoke-RestMethod -Uri $jsonUrl -Method GET
+    $latestVersion = $json.dist.latest
+    $latestUrl = $json.versions.$latestVersion.win
+    Write-Output $json
+    Write-Host "Latest available version: $latestVersion"
+
+    $TargetDir = "C:\Program Files\Autoclicker"
+    $DownloadPath = Join-Path -Path $TargetDir -ChildPath "autoclick.exe"
+
+    $currentPath = [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::Machine)
+    if ($currentPath -split ";" -contains $TargetDir) {
+        Write-Host "Autoclick is already in PATH. Updating executable..."
+    } else {
+        Write-Host "Autoclick is not in PATH. Adding it now..."
+        [Environment]::SetEnvironmentVariable("PATH", "$currentPath;$TargetDir", [EnvironmentVariableTarget]::Machine)
+        Write-Host "Autoclick added to PATH."
+    }
+
+    if (-not (Test-Path -Path $TargetDir)) {
+        New-Item -ItemType Directory -Path $TargetDir -Force
+    }
+
+    Invoke-WebRequest -Uri $latestUrl -OutFile $DownloadPath
+    Write-Output "Autoclick successfully installed/updated."
+
+} catch {
+    Write-Host "Error fetching registry information: $_"
+}
